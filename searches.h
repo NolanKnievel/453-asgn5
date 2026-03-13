28bb71e0bfe9169cd1e50a9eb0a2e4294b3eae44
joshdagan05 <155996824+joshdagan05@users.noreply.github.com>
2026-03-12 22:38:58 -0700

    Revert "working?"

--- searches.h
+++ searches.h
@@ -3,21 +3,44 @@
 
 #include "reads.h"
 
-#define POINTER_SIZE_BYTES 4
+int file_search_datazone(int zonesize,
+        struct directory* zone_data,
+        unsigned char* target,
+        struct directory* dir
+        );
 
-uint32_t get_file_zone(int fd, struct superblock *sb, 
-    struct inode *node, uint32_t index, uint32_t fs_start);
 
-int search_file_zone(unsigned char* target, 
-    struct directory* dirs, uint32_t zone_size, 
-    struct directory* final_dir);
+int search_all(int fd,
+    Config* config, 
+    uint16_t blocksize,
+    struct inode* inode,
+    int inode_start, 
+    int zonesize, 
+    struct directory* final_dir,
+    struct inode* final_inode
+    );
 
-int search_zones(int fd, struct superblock* sb,
-     struct inode* inode, struct directory* final_dir, 
-     unsigned char *target, int fs_start);
+int search_direct(int fd, 
+    struct inode* parent, 
+    Config* config, 
+    int zonesize,
+    int inode_start,
+    struct directory* final_dir
+    );
 
-int search_all(int fd, struct superblock *sb, 
-    struct inode *node, uint32_t fs_start, uint32_t inode_start,
-     char* path, struct directory* final_dir, struct inode* final_inode);
+// int search_indirect(int fd, 
+//     struct inode* inode,
+//     Config* config, 
+//     uint16_t blocksize,
+//     int zonesize, 
+//     struct directory* final_dir
+//     );
 
+// int search_2x_indirect(int fd, 
+//     struct inode* inode,
+//     Config* config, 
+//     uint16_t blocksize,
+//     int zonesize, 
+//     struct directory* final_dir
+//     );
 #endif
